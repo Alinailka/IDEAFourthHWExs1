2e2bfcf6d54644fe2b896d3f579a7163d462ea55
Alinailka <alina-il@mail.ru>
2022-03-06 16:36:57 +0700

    More data.csv

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -6,7 +6,47 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BonusServiceTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/data.csv")
-    void shouldCalculateBonus(long amount, long expected, boolean registered) {
+    void shouldCalculateForRegisteredAndUnderLimit(long amount, long expected, boolean registered) {
+        BonusService service = new BonusService();
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data1.csv")
+    void shouldCalculateForRegisteredAndOverLimit(long amount, long expected, boolean registered) {
+        BonusService service = new BonusService();
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data2.csv")
+    void shouldCalculateForRegisteredAndEqualLimit(long amount, long expected, boolean registered) {
+        BonusService service = new BonusService();
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data3.csv")
+    void shouldCalculateForNoRegisteredAndUnderLimit(long amount, long expected, boolean registered) {
+        BonusService service = new BonusService();
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data4.csv")
+    void shouldCalculateForNoRegisteredAndOverLimit(long amount, long expected, boolean registered) {
+        BonusService service = new BonusService();
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data5.csv")
+    void shouldCalculateForNoRegisteredAndEqualLimit(long amount, long expected, boolean registered) {
         BonusService service = new BonusService();
         long actual = service.calculate(amount, registered);
         assertEquals(expected, actual);
